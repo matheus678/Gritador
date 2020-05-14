432929457c29d62d3b96aac856e4359a63db7ba1
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-14 01:20:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva qualquer coisa: ");
+            string texto = Console.ReadLine();
+
+            Console.Write (texto.ToUpper());
+        }
+    }
+}
